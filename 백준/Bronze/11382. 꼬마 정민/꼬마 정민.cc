d12c15273c88a0f292ef717c26c6b77ed3f23b98
_GGDol <bohg6799@naver.com>
2026-03-09 19:23:06 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long a,b,c;
+    cin >> a >> b >> c;
+    cout << (long long)a + b + c << "\n";
+
+    return 0;
+}
