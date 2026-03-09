8537fdbe0d536a123a15978115ee31fdd9fcfc51
_GGDol <bohg6799@naver.com>
2026-03-09 19:16:56 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int num1, num2;
+    cin >> num1 >> num2;
+    cout << num1 * (num2 % 10) << "\n";
+    cout << num1 * ((num2 % 100) / 10) << "\n";
+    cout << num1 * (num2 / 100) << "\n";
+    cout << num1 * num2 << "\n";
+
+    return 0;
+}
