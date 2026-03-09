42d649a217b257400b989997158ad882aed18045
_GGDol <bohg6799@naver.com>
2026-03-09 15:36:28 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    double a,b;
+    cin >> a >> b;
+    cout << fixed << setprecision(9) << a / b << endl;
+
+    return 0;
+}
