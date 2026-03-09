80bc082e2b15f19b169f28303b1a369344e121cb
_GGDol <bohg6799@naver.com>
2026-03-09 18:53:44 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int year;
+    cin >> year;
+    cout << year - 543 << "\n";
+
+    return 0;
+}
