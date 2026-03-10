d437a5b26a31082cad87550314d19870ae0edfde
_GGDol <bohg6799@naver.com>
2026-03-10 19:15:12 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -6,25 +6,34 @@ int main() {
     cin.tie(NULL);
 
     int a, b, c;
-    
     cin >> a >> b >> c;
 
-    // 규칙 1
-    if ((a == b) && (b == c)) {
-        cout << 10000 + (a * 1000);
-    }
+    vector<int> v = {a, b, c};
+    sort(v.begin(), v.end());
 
-    // 규칙 2
-    else if ((a == b) || (a == c)) {
-        cout << 1000 + a * 100;
-    } else if (b == c) {
-        cout << 1000 + b * 100;
-    }
+    if (v[0] == v[2])
+        cout << 10000 + v[0] * 1000;
+    else if (v[0] == v[1] || v[1] == v[2])
+        cout << 1000 + v[1] * 100;
+    else
+        cout << v[2] * 100;
 
-    // 규칙 3
-    else {
-        cout << max({a, b, c}) * 100;
-    }
+    // // 규칙 1
+    // if ((a == b) && (b == c)) {
+    //     cout << 10000 + (a * 1000);
+    // }
+
+    // // 규칙 2
+    // else if ((a == b) || (a == c)) {
+    //     cout << 1000 + a * 100;
+    // } else if (b == c) {
+    //     cout << 1000 + b * 100;
+    // }
+
+    // // 규칙 3
+    // else {
+    //     cout << max({a, b, c}) * 100;
+    // }
 
     return 0;
 }
